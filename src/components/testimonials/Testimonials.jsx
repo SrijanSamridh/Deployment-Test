b9b8bf79bab9904959922b07965f671b25ccd29b
Srijan Samridh <srijansinha3@gmail.com>
2023-02-11 01:13:36 +0530

    Test

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './testimonials.css'
+
+const Testimonials = () => {
+  return (
+    <section id='testimonials'>
+      <h5></h5>
+      <h2>Testimonials</h2>
+    </section>
+  )
+}
+
+export default Testimonials
